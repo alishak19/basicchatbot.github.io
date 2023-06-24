6209e5069bab34fff40265ef7d391dc1efdc0a21
Alisha Kumar <93096342+alishak19@users.noreply.github.com>
2023-06-24 17:08:22 -0500

    Update Bot.java

--- Bot.java
+++ Bot.java
@@ -1,7 +1,11 @@
 public class Bot extends JFrame {
 
     public Bot() {
-        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLayout(null);
 
     }
 
