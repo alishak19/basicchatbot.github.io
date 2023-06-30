8ad23bc0482ccd4033ffb14d57c2ff50e9f1d761
Alisha Kumar <93096342+alishak19@users.noreply.github.com>
2023-06-29 21:22:43 -0500

    Update RulesBot.java

--- RulesBot.java
+++ RulesBot.java
@@ -11,7 +11,7 @@ public class RulesBot extends JFrame {
         frame.setResizable(true);
         frame.setLayout(null);
 
-        frame.setSize(559, 550);
+        frame.setSize(558, 550);
 
     }
 
