4dfb42e1e7c5421381b3ed9cb15554114171a955
Alisha Kumar <93096342+alishak19@users.noreply.github.com>
2023-07-26 21:14:59 -0500

    Update LearningBot.py

--- LearningBot.py
+++ LearningBot.py
@@ -1,6 +1,6 @@
 import transformers
 
-import speech_recognition as sr
+import speech_recognition as sre
 from gtts import gTTS
 
 import os
@@ -9,4 +9,21 @@ import time
 import numpy as np
 import datetime
 
+# creating the AI model bot
+
+class LearningBot(): 
+  def_init_(self, name): 
+    print("Starting up", name, "")
+    self.name = name
+  def speech_to_text(self):
+    recognizer = sre.Recognizer()
+    with sre.Microphone() as micdrop:
+      print("Listening to you...")
+      audio = recognizer.listen(micdrop)
+      self.text="ERROR"
+    try: 
+      self.text = recognizer.recognize_google(audio)
+      print("who me?? ", self.text)
+    except:
+      print("who me?? ERROR")
 
