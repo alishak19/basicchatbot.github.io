396fdc2862865560555e2e8eccd83741bb80d77d
Alisha Kumar <93096342+alishak19@users.noreply.github.com>
2023-07-29 11:21:03 -0500

    Update LearningBot.py

--- LearningBot.py
+++ LearningBot.py
@@ -13,7 +13,7 @@ import datetime
 
 class LearningBot(): 
   def_init_(self, name): 
-    print("waking u", name, "")
+    print("waking ", name, "")
     self.name = name
   def speech_to_text(self):
     recognizer = sre.Recognizer()
