6d3f850d4562f4faa8c515179709ca2a35121d56
Alisha Kumar <93096342+alishak19@users.noreply.github.com>
2023-08-17 22:46:44 -0500

    Update LearningBot.py

--- LearningBot.py
+++ LearningBot.py
@@ -42,7 +42,7 @@ class LearningBot():
   @staticmethod
   def action_time(): 
     return datetime.datetime.now().time().strftime('%H:%M')
-#turn the ai into a runner/track star
+#turn the ai into a runner/track sta
 if __name__ == "__main__":
   ai = LearningBot(name="dev")
   nlp = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium")
